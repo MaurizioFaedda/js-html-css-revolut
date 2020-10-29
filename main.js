6795081bd58178fd330fd90ff63c94eb8726fe73
Maurizio <maurizio.faedda1@gmail.com>
2020-10-29 04:13:21 +0100

    index/eq to dropbox

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 
-    $('#funzionalita').click(function() {
-        $('#full-dropdown').toggle();
-    });
+
+    $('.title-menu').click(function() {
+
+        var indice_title = $(this).index();
+        var new_drop = $('.my-dropdown').eq(indice_title);
+
+        // con hide quando apro un nuovo dropdown quello corrente si chiude, ma non si chiude se riclicco nel suo title
+        $('.my-dropdown').hide();
+        // senza hide si chiude se faccio click nel suo title ma non se faccio click in un altro
+        new_drop.slideToggle();
+
+        // con add e remove se clicco sullo stesso title non si chiude
+        // new_drop.addClass('active');
+        // $('.my-dropdown').removeClass('active');
+    })
+
 
 });
